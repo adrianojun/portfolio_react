b4d13dc6a5d1d3cca86b0b18ef64228d3cc20976
Adriano Júnior <adrianojunnior21@gmail.com>
2024-06-07 16:44:59 -0300

    Configurações de deploy

--- src/components/TechnologiesContainer.tsx
+++ src/components/TechnologiesContainer.tsx
@@ -14,7 +14,7 @@ const technologies = [
   { id: "css", name: "CSS3", icon: <DiCss3 />, description:"Conhecimentos intermediários em CSS" },
   { id: "python", name: "Python", icon: <DiPython />, description:"Conhecimentos intermeiários em python" },
   { id: "node", name: "Node.js", icon: <DiNodejsSmall />,description:"Conhecimentos básicos em node" },
-  { id: "mysql", name: "MySQL", icon: <DiMysql />, description:"Conhecimentos básicos em mysql" },
+  { id: "mysql", name: "MySQL", icon: <DiMysql />, description:"Conhecimentos básicos em  MySQL" },
   { id: "react", name: "React", icon: <DiReact />, description:"Conhecimentos básicos em React" },
 ];
 
